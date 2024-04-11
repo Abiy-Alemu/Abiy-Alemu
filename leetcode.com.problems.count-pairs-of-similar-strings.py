4473c6aa39b73e4eeaab08eba7232a92db6a3400
Abiy-Alemu <105849228+Abiy-Alemu@users.noreply.github.com>
2024-04-11 22:38:00 +0300

    Add files via upload

---
+++ leetcode.com.problems.count-pairs-of-similar-strings.py
@@ -0,0 +1,14 @@
+class Solution:
+    def similarPairs(self, words: List[str]) -> int:
+        count =0
+         
+        for i in range(len(words)-1):
+            for j in range(i+1,len(words)):
+                set1 = set(words[i])
+                set2 = set(words[j])
+
+                if set1 ==set2:
+                    count +=1
+
+        return count
+
