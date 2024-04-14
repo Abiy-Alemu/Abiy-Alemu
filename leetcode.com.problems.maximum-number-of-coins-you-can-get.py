50037dff9c3a31d4700706d10bafbd2baa883808
Abiy-Alemu <105849228+Abiy-Alemu@users.noreply.github.com>
2024-04-14 10:24:37 +0300

    Add files via upload

---
+++ leetcode.com.problems.maximum-number-of-coins-you-can-get.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxCoins(self, piles: List[int]) -> int:
+        piles.sort()
+        queue = deque(piles)
+        ans = 0
+        while queue:
+            queue.pop() 
+            ans += queue.pop() 
+            queue.popleft() 
