5faf524c8872582c5019365bb21fb77a59909ca3
Abiy-Alemu <105849228+Abiy-Alemu@users.noreply.github.com>
2024-05-25 10:06:17 +0300

    Add files via upload

--- Maximum Matching of Players With Trainers.py
+++ Maximum Matching of Players With Trainers.py
@@ -3,18 +3,21 @@ class Solution:
         
         players.sort()
         trainers.sort()
+
+        count =0 
+        pp = 0 # players_pointer
+        tp  = 0 # trainers_pointer
+        n = len(players)
+        m  = len(trainers)
         
-        player_pointer = 0
-        trainer_pointer = 0
-        count = 0
-        
-    
-        while player_pointer < len(players) and trainer_pointer < len(trainers):
-            if players[player_pointer] <= trainers[trainer_pointer]:
-                count += 1
-                player_pointer += 1
-            trainer_pointer += 1
-        
+
+        while pp < n and tp < m:
+            if players[pp] <= trainers[tp]:
+                count  += 1
+                pp += 1
+            tp += 1
+
         return count
+
 
         
